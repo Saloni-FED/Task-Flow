39c7e38700660c3bdfc1c668d240e5961c540cfc
Saloni Pandey <salonipandey014@gmail.com>
2023-12-12 02:19:27 +0530

    setup basic server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("node js")
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Server is started")
+})
+app.listen(3000,()=>{
+    console.log("app is listening at post 3000")
+})
