83941c25d3ff5b1f48376dacb7306e442014f024
Saloni Pandey <salonipandey014@gmail.com>
2023-12-12 20:29:31 +0530

    connected server and database and fixed routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 import express from "express";
+import dotenv from "dotenv";
+import connect from "./db/connect.js";
+dotenv.config();
+
+// console.log(process.env.MONGODB_URI)
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Server is started")
-})
-app.listen(3000,()=>{
-    console.log("app is listening at post 3000")
-})
+app.get("/", (req, res) => {
+  res.send("Server is started");
+});
+
+const start = async () => {
+  try {
+    await connect(process.env.MONGODB_URI);
+    app.listen(3000, () => {
+      console.log("app is listening at post 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
